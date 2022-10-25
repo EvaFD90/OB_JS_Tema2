5b3c5d1a074ab9a3a2f30e99c8a0a80cd61ff591
EvaFD90 <evafilgueirasaxo@gmail.com>
2022-10-25 14:25:52 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const list = [
+    "Eva",
+    32,
+    true,
+    new Date(1990, 5, 28),
+    {
+        title: "Transmetropolitan",
+        author: "Warren Ellis",
+        date: new Date(1997, 0, 1),
+        url: "https://www.tebeosfera.com/sagas/transmetropolitan_1997_ellis_robertson.html",
+    },
+];
